ef6344b536e7f10790ef85405a4f6f2a6d3567fc
‘komalata005’ <Komal.ata005@gmail.comgit config --global user.email Komal.ata005@gmail.com>
2023-10-15 12:50:48 +0500

    buttons and alert done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Alert from './components/Alerts/Alert';
+import Button from './components/Button/Button'
 
 function App() {
   return (
-    <h1>Tailwind UI APP</h1>
+    <>
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-5">
+      {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
+      <div className="md:flex md:items-center md:justify-between">
+      <div className="min-w-0 flex-1">
+        <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+        Tailwind UI APP
+
+        </h2>
+      </div>
+      {/* <div className="mt-4 flex md:ml-4 md:mt-0">
+        <button
+          type="button"
+          className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        >
+          Edit
+        </button>
+        <button
+          type="button"
+          className="ml-3 inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Publish
+        </button>
+      </div> */}
+    </div>
+      <Alert />
+      <Button />
+
+      <div className="mx-auto max-w-3xl">{/* Content goes here */}</div>
+    </div>
+    </>
   );
 }
 
--- src/components/Alerts/Alert.tsx
+++ src/components/Alerts/Alert.tsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import { AiFillExclamationCircle } from "react-icons/ai";
+import { MdCancel} from "react-icons/md";
 
 const Alert = () => {
   return (
-    <div>Alert</div>
+    <>
+       <div className="border-b border-gray-200 pb-5 my-4">
+      <h3 className="text-base font-semibold leading-6 text-gray-900 ">Alert</h3>
+    </div>
+    <div className="rounded-md bg-yellow-50 p-4 w-25">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <AiFillExclamationCircle className="h-5 w-5 text-yellow-400" aria-hidden="true" />
+        </div>
+        <div className="ml-3">
+          <h3 className="text-sm font-medium text-yellow-800">Attention needed</h3>
+          <div className="mt-2 text-sm text-yellow-700">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo
+              totam eius aperiam dolorum.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br/>
+    <div className="rounded-md bg-red-50 p-4">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <MdCancel className="h-5 w-5 text-red-400" aria-hidden="true" />
+        </div>
+        <div className="ml-3">
+          <h3 className="text-sm font-medium text-red-800">There were 2 errors with your submission</h3>
+          <div className="mt-2 text-sm text-red-700">
+            <ul role="list" className="list-disc space-y-1 pl-5">
+              <li>Your password must be at least 8 characters</li>
+              <li>Your password must include at least one pro wrestling finishing move</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
