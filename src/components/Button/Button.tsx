8645fb50092949901b5e8ce2c107aa7a1c9f0524
Komal Ata <komal.ata005@gmail.com>
2023-10-15 13:04:29 +0500

    buttons and alert done (#2)
    Co-authored-by: ‘komalata005’ <‘Komal.ata005@gmail.comgit config --global user.email ‘Komal.ata005@gmail.com>

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,86 @@
 import React from 'react'
+import { BiChevronRight} from "react-icons/bi";
+import { BiChevronLeft} from "react-icons/bi";
 
 const Button = () => {
   return (
-    <h2>Tailwind button</h2>
+    <>
+        <div className="border-b border-gray-200 pb-5 my-4">
+      <h3 className="text-base font-semibold leading-6 text-gray-900 ">Button</h3>
+    </div>
+    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-6">
+    <button
+      type="button"
+      className="rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    >
+      Button text
+    </button>
+    <button
+      type="button"
+      className="rounded bg-indigo-600 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    >
+      Button text
+    </button>
+    <button
+      type="button"
+      className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    >
+      Button text
+    </button>
+    <button
+      type="button"
+      className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    >
+      Button text
+    </button>
+    <button
+      type="button"
+      className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    >
+      Button text
+    </button>
+    </div>
+    <br/>
+    <span className="isolate inline-flex rounded-md shadow-sm">
+      <button
+        type="button"
+        className="relative inline-flex items-center rounded-l-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+      >
+        Years
+      </button>
+      <button
+        type="button"
+        className="relative -ml-px inline-flex items-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+      >
+        Months
+      </button>
+      <button
+        type="button"
+        className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+      >
+        Days
+      </button>
+    </span>
+    <br />
+    <br />
+
+    <span className="isolate inline-flex rounded-md shadow-sm">
+      <button
+        type="button"
+        className="relative inline-flex items-center rounded-l-md bg-white px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+      >
+        <span className="sr-only">Previous</span>
+        <BiChevronLeft className="h-5 w-5" aria-hidden="true" />
+      </button>
+      <button
+        type="button"
+        className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+      >
+        <span className="sr-only">Next</span>
+        <BiChevronRight className="h-5 w-5" aria-hidden="true" />
+      </button>
+    </span>
+  </>
   )
 }
 
