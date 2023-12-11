55f438604bc4d544910cc1b2d3d5426e3f5bb25c
Maham Ilyas <mahamilyas211@gmail.com>
2023-12-11 18:39:35 +0500

    HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
-import Alert from './components/Alerts/Alert';
-import Cards from './components/Cards/Cards';
-import Button from './components/Button/Button';
-import InputField from './components/InputFields/InputField';
-import Textarea from './components/Textarea/Textarea';
-import IconCard from './components/Cards/IconCard';
-import PicCards from './components/Cards/PicCards';
-import Tables from './components/Tables/spanningTable';
-import Sidebar from './components/Sidebar/Sidebar';
+
+import  HomePage from './components/HomePage/HomePage';
+{/*import PicCards from './components/Cards/PicCards';*/}
+{/*import Tables from './components/Tables/DenseTable';*/}
+{/*import Alert from './components/Alerts/Alert';*/}
+{/*import Cards from './components/Cards/Cards';*/}
+{/*import Button from './components/Button/Button';*/}
+{/*import InputField from './components/InputFields/InputField';*/}
+{/*import Textarea from './components/Textarea/Textarea';*/}
+{/*import IconCard from './components/Cards/IconCard';*/}
+{/*import Sidebar from './components/Sidebar/Sidebar'; */}
+
+
+
 
 
 
@@ -20,21 +25,24 @@ function App() {
       {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
       <div className="md:flex md:items-center md:justify-between">
       <div className="min-w-0 flex-1">
-        <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+        {/*<h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight text-center">
         Tailwind UI APP
 
-        </h2>
+        </h2>*/}
       </div>
     </div>
-      <Alert />
-      <Button />
-      <Cards />
-      <IconCard />
-      <Textarea />
-      <InputField />
-      <PicCards />
-      <Tables />
-      <Sidebar/>
+     {/* <Alert />*/}
+      {/*<Button />*/}
+      {/*<Cards />*/}
+     {/* <IconCard />*/}
+     {/* <Textarea />*/}
+      {/*<InputField />*/}
+      {/*<Tables />*/}
+      {/*<Sidebar/> */}
+      {/*<PicCards />*/}
+      <HomePage/>
+      
+      
 
     </div>
     </>
