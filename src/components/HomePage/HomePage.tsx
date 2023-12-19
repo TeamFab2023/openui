90693fe7f05d62a0b27269d9c65c6ccce1ea9e25
Rafia Zeeshan <Rafiazeeshan2114@gmail.com>
2023-12-19 22:36:59 +0500

    HomePage-Completed

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,35 +1,59 @@
+import React from 'react';
+import Sidebar from '../Sidebar/Sidebar';
 import Table from '../Tables/DenseTable';
-import Layout from '../Layouts/Layout';
+import Cards from '../Cards/Cards';
+import PicCards from '../Cards/PicCards';
+import IconCard from '../Cards/IconCard';
+import Alert from '../Alerts/Alert';
+import Button from '../Button/Button';
+import Textarea from '../Textarea/Textarea';
+import InputField from '../InputFields/InputField';
+import BasicTable from '../Tables/StickyHeaderTable';
+import DenseTable from '../Tables/DenseTable';
 
 
 const HomePage = () => {
   return (
-    <Layout>
-      <div style={{ display: 'grid', gridTemplateColumns: '10px 1fr', gap: '100px' }}>
-        {/* HomePage */}
-        <div>
-          {/* Main content of the Homepage */}
+    <>
+    
+    <div style={{ display: 'grid', gridTemplateColumns: '0px 1fr', gap: '' }}>
+      
+      {/* SideBar */}
+      <div>
 
-          <h1 className='text-center no-underline hover:underline decoration-red-300 decoration-2 text-5xl font-bold text-blue-700 
-        hover:cursor-pointer'>HomePage</h1>
-          <div className='text-start'>
-            <h2 className='mt-10 font-bold text-3xl'>Heading 1:</h2>
-            <p className='mt-2 text-lg italic font-normal'>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti dolor deleniti vel delectus possimus
-              quisquam sunt saepe maiores, magnam esse adipisci vero, nostrum iste expedita quam autem. Beatae, excepturi alias?
-            </p>
-          </div>
-          <br />
-          <br />
-          <br />
-          {/*<Cards/>*/}
-          <Table />
-
-
-          {/* Add more content here */}
-        </div>
+        <Sidebar />
       </div>
-    </Layout>
+
+      {/* HomePage */}
+      <div>
+        {/* Main content of the Homepage */}
+
+        <h1 className=' sm:ml-[150px] p-2 text-center no-underline hover:underline decoration-red-300 decoration-2 text-5xl font-bold text-blue-700 
+        hover:cursor-pointer'>HOMEPAGE</h1>
+        <div className='text-start sm:ml-64 p-3'>
+          <h2 className='mt-10 font-bold text-3xl'>OPENING:</h2>
+          <p className='mt-2 text-lg italic font-normal'>
+          Welcome to <b>Materialize UI</b> – your ultimate destination for high-quality components that power innovation. At <b>Materialize UI</b>, 
+          we understand the importance of reliable, top-notch components to fuel your projects. Explore our comprehensive catalog, where each component is meticulously curated to ensure performance, durability, and above all, your satisfaction. Join us on this journey of innovation as we provide the building blocks for your next big idea. </p>
+        </div>
+        <br/>
+       
+        <br/>
+        <IconCard/>
+        <DenseTable/>
+        <br/>
+        <br/>
+       
+      
+        
+        <PicCards/>
+      
+        
+        
+      </div>
+      
+    </div>
+    </>
   );
 };
 
