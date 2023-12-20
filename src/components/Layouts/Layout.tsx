90f707a4a98e899db74abad470e0786c75e83d57
Rafia Zeeshan <141746940+RafiaZeeshan14@users.noreply.github.com>
2023-12-20 20:25:10 +0500

    HomePage-Completed (#29)
    Co-authored-by: Rafia Zeeshan <Rafiazeeshan2114@gmail.com>

--- src/components/Layouts/Layout.tsx
+++ src/components/Layouts/Layout.tsx
@@ -1,8 +1,18 @@
+
 import Sidebar from "../Sidebar/Sidebar"
 
 const Layout = ({children}: any) => {
   return (
-      <><Sidebar /><div>{children}</div></>
+      <>
+      <div className="grid grid-cols-2 ">
+ 
+         <Sidebar />
+   
+ 
+         <div className=" p-4 sm:ml-64 grid col-span-2 ">
+           <div>{children}</div>
+         </div>
+       </div></>
   )
 }
 export default Layout
