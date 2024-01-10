24470494396ee06a8e6c7f8f50ffda1e672e3738
Binish Farhan <binishfarhan89@gmail.com>
2024-01-10 21:11:40 +0500

    Tables are modified and showing each table on click.

--- src/App.tsx
+++ src/App.tsx
@@ -8,37 +8,42 @@ import Button from './components/Button/Button';
 import InputField from './components/InputFields/InputField';
 import Textarea from './components/Textarea/Textarea';
 import IconCard from './components/Cards/IconCard';
-import Tables from './components/Tables/spanningTable';
+// import Tables from './components/Tables/BasicTable';
 import PicCards from './components/Cards/PicCards';
 import BasicTable from './components/Tables/BasicTable';
 import DenseTable from './components/Tables/DenseTable';
 import HomePage from './components/HomePage/HomePage';
+import StickyHeaderTable from './components/Tables/StickyHeaderTable'
+import SpanningTable from './components/Tables/SpanningTable'
 
 function App() {
   return (
     // <div>aooa</div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
 
-            <Route path="/alerts" element={<Alert />} />
-            <Route path="button" element={<Button />} />
+        <Route path="/alerts" element={<Alert />} />
+        <Route path="button" element={<Button />} />
 
-             <Route path="cards" element={<Cards />}>
-              <Route path="iconcard" element={<IconCard />} />
-              <Route path="piccards" element={<PicCards />} />
-            </Route>
+        <Route path="cards" element={<Cards />}>
+          <Route path="iconcard" element={<IconCard />} />
+          <Route path="piccards" element={<PicCards />} />
+        </Route>
 
-            <Route path="inputfields" element={<InputField />} />
+        <Route path="inputfields" element={<InputField />} />
 
-            <Route path="tables" element={<Tables />}>
-              <Route path="BasicTable" element={<BasicTable />} />
-              <Route path="DenseTable" element={<DenseTable />} />
-            </Route>
+        {/* <Route path="tables" element={<Tables />}> */}
+          <Route path="/Tables/BasicTable" element={<BasicTable />} />
+          <Route path="/Tables/DenseTable" element={<DenseTable />} />
+          <Route path="/Tables/StickyHeaderTable" element={<StickyHeaderTable />} />
+          <Route path="/Tables/SpanningTable" element={<SpanningTable />} />
+        {/* </Route> */}
 
-            <Route path="Textarea" element={<Textarea />} />
-        </Routes>
-      </BrowserRouter>
+
+        <Route path="Textarea" element={<Textarea />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
