90c82eeafd1aa455465ca2d32682f0e824086f55
Mahamilyas211 <mahamilyas211@gmail.com>
2024-02-20 19:46:13 +0300

    Simple Footer Added

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <div className="  relative overflow-hidden bg-gray-200   text-center py-4  max-w-full  ">
+      <div className=" mx-w-auto   md:max-w-full md:ml-72 lg:ml-62 xl:ml-96 ">
+          <p  className=" text-black text-xs font-semibold  "  > ©2024. | Designed By: <span className='text-blue-400 font-bold'>Team Fab</span>   | All rights reserved.
+          </p>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default Footer
